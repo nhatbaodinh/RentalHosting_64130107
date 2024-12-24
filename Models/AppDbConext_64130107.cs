839cd68e9d9a632ee24c6386bf324fb703f49ad2
nhatbaodinh <dinhnhatbao.work@gmail.com>
2024-12-25 01:14:26 +0700

    commit

--- Models/AppDbConext_64130107.cs
+++ Models/AppDbConext_64130107.cs
@@ -7,7 +7,6 @@ public class AppDbContext : DbContext
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
     public DbSet<NguoiDungModel_64130107> NguoiDung { get; set; }
-    public DbSet<LoaiHostingModel_64130107> LoaiHosting { get; set; }
     public DbSet<HostingModel_64130107> Hosting { get; set; }
     public DbSet<HopDongModel_64130107> HopDong { get; set; }
     public DbSet<ChiTietHopDongModel_64130107> ChiTietHopDong { get; set; }
